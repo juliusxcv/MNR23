ebe617955a19a18db28f43b6db1a658022c83e2d
juliusxcv <julius.xcv@gmail.com>
2023-11-06 17:55:26 +0100

    boing

--- mnr23.js
+++ mnr23.js
@@ -6,10 +6,12 @@ let dialangle = 0
 
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    pixelDensity(1)
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('mnr23-container');
     noiseSeed(04679836528934789); // set the noise seed for consistency
     for (let i = 0; i < 20; i++) {
-        let radius = map(noise(i * 0.5), 0.1, 1, 0.1, 500); // use Perlin noise for radius
+        let radius = map(noise(i * 0.5), 0.1, 1, 0.1, 500); //  Perlin noise for dispersion
         let lines = [];
         for (let j = 0; j < random(1, 5); j++) {
             lines.push({
@@ -20,6 +22,8 @@ function setup() {
         objects.push(new Orbiter(radius, lines));
     }
 
+
+
     
 }
 
@@ -28,16 +32,16 @@ function draw() {
     background(0);
 
 
-
-
     fill(0, 0, 0, 0); // set fill to white
     stroke(255); // set stroke to black
     strokeWeight(1); // set stroke weight to 1
+
 
     // Draw the objects
     for (let i = 0; i < objects.length; i++) {
         objects[i].draw();
     }
+
 
 
     //Circle Anim
@@ -65,6 +69,28 @@ function draw() {
     }
 
     
+    push()
+    strokeWeight(0);
+    blendMode(EXCLUSION);
+    fill(255)
+    ellipse(0, 0, 500, 500);
+    pop()
+
+    //Cursor
+    push()
+    strokeWeight(0);
+    blendMode(DARKEST);
+    fill (255,0,0)
+    ellipse(mouseX-width/2, mouseY-height/2, 300, 300);
+    pop()
+
+    fill(0)
+    strokeWeight(0)
+    stroke(0)
+    textSize(100)
+    textFont('Arial Black')
+    text("23", 0, 0);
+
     
 
     // TYPO
@@ -72,34 +98,34 @@ function draw() {
     stroke(255)
     textAlign(CENTER, CENTER);
 
-    fill(0,0,0,0)
-    strokeWeight(2)
-    textSize(180)
-    textFont('Arial Black')
-    text("23", 0, 0);
+    // strokeWeight(2)
+    // stroke(255)
+    // textSize(70);
+    // textFont('Arial Black');
+    // fill(255)
+    // text("MIDNIGHT RUN", 0, 0);
 
 
-    // fill(0)
-    strokeWeight(2)
-    textSize(70);
-    textFont('Arial Black');
-    fill(0)
-    text("MIDNIGHT RUN", 0, 0);
 
-    fill(255)
-    strokeWeight(0)
-    textSize(20);
-    textFont('Arial');
-    text("22.11.2023", 0, 0-height/2+20);
+    // fill(255)
+    // strokeWeight(0)
+    // textSize(20);
+    // textFont('Arial');
+    // text("22.11.2023", 0, 0-height/2+20);
 
-    textAlign(LEFT, CENTER)
-    textSize(20);
-    textFont('Arial');
-    text("START Eibenstr", 0-width/2+20, 0+height/2-20);
+    // textAlign(LEFT, CENTER)
+    // textSize(20);
+    // textFont('Arial');
+    // text("START Eibenstr", 0-width/2+20, 0+height/2-20);
 
-    textAlign(RIGHT, CENTER)
-    textSize(20);
-    textFont('Arial');
-    text("END Wasserwerkstr", 0+width/2-20, 0+height/2-20);
+    // textAlign(RIGHT, CENTER)
+    // textSize(20);
+    // textFont('Arial');
+    // text("END Wasserwerkstr", 0+width/2-20, 0+height/2-20);
 
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
